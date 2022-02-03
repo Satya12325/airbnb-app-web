38120b1bbd851dec4598449a7d9566792703b3a3
Satyabrata Das <satyabrata_pw1_135@masai.school>
2022-02-04 01:58:35 +0530

    for deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import AllpageRout from './Router/AllpageRout';
+
+function App() {
+  return (
+    <div className="App">
+      <AllpageRout/>
+    </div>
+  );
+}
+
+export default App;
